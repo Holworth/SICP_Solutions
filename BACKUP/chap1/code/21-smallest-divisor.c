2d4748c5730a8e9162c20cffe0843b53bc9ebdd3
Holworth <kangqihan17@mails.ucas.ac.cn>
2021-12-23 12:54:51 +0800

    Move scheme based code into BACKUP directory

--- BACKUP/chap1/code/21-smallest-divisor.c
+++ BACKUP/chap1/code/21-smallest-divisor.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-#include<math.h>
+#include <math.h>
+#include <stdio.h>
 
-int smallest_divisor(int n){
-   double t = sqrt(n);
-   for(int i = 2; i <= t; ++i){
-	if(n % i == 0) return i;
-   }
-   return n;
+int smallest_divisor(int n) {
+  double t = sqrt(n);
+  for (int i = 2; i <= t; ++i) {
+    if (n % i == 0)
+      return i;
+  }
+  return n;
 }
 
-int main(){
+int main() {
   printf("smallest divisor for 199   is %3d\n", smallest_divisor(199));
   printf("smallest divisor for 1999  is %3d\n", smallest_divisor(1999));
   printf("smallest divisor for 19999 is %3d\n", smallest_divisor(19999));
   return 0;
 }
- 
