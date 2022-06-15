4e693c46812fd5a1968bb2f52cad1f1502e91d49
Holworth <kangqihan17@mails.ucas.ac.cn>
2022-06-15 14:44:33 +0800

    Add new solutions

---
+++ solutions/chap2/code/exercise2-68.rkt
@@ -0,0 +1,73 @@
+#lang sicp
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+                (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+  (define (encode-symbol-1 s result branch)
+    (if (leaf? branch)  
+        result
+        (cond ((element-of-set? s (symbols (left-branch branch)))
+                (encode-symbol-1 s (append result (list 0)) (left-branch branch)))
+              ((element-of-set? s (symbols (right-branch branch)))
+                (encode-symbol-1 s (append result (list 1)) (right-branch branch))))))
+(define (encode-symbol s tree)
+  (encode-symbol-1 s '() tree))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? (car set) x) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                    (make-leaf 'B 2)
+                    (make-code-tree (make-leaf 'D 1)
+                                    (make-leaf 'C 1)))))
+
+(encode (list 'A 'D 'A 'B 'B 'C 'A) sample-tree)
